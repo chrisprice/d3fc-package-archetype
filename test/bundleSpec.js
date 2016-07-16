6122368bf2ff27e277020594d0a5f7815d25a6ee
Colin Eberhardt <colin.eberhardt@gmail.com>
2016-07-16 08:15:33 +0100

    test: added a test for the bundle

---
+++ test/bundleSpec.js
@@ -0,0 +1,20 @@
+import jsdom from 'jsdom';
+
+describe('bundle', function() {
+    it('should corectly wire-up all the dependencies via their UMD-exposed globals', function(done) {
+        jsdom.env({
+            html: '<html></html>',
+            virtualConsole: jsdom.createVirtualConsole().sendTo(console),
+            scripts: [
+                // include and other dependencies here ...
+                './build/d3fc-package-archetype.js'
+            ],
+            done: (_, win) => {
+                // simple exercise a code-path that includes all the dependencies
+                const result = win.fc.archetype();
+                expect(result).not.toBeUndefined();
+                done();
+            }
+        });
+    });
+});
